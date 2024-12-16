60d2b375cc89716d94bd7b8ce07508b28b8ffa97
Just-Hosam <hosam_firas@hotmail.com>
2024-12-16 02:56:27 -0700

    events empty state

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -4,6 +4,7 @@ import prisma from "@/lib/prisma"
 import { Event } from "@prisma/client"
 import { PlusIcon } from "lucide-react"
 import { getServerSession } from "next-auth"
+import Image from "next/image"
 import Link from "next/link"
 import { redirect } from "next/navigation"
 
@@ -36,18 +37,38 @@ export default async function Events() {
         </Link>
       </header>
       <div>
-        {events.map((event: Event) => (
-          <Link href={`/events/${event.id}`} key={event.id}>
-            <div className="border rounded-2xl p-4 mb-3">
-              <h3 className="text-xl font-semibold">{event.name}</h3>
-              {event?.description && (
-                <p className="whitespace-pre-line text-light text-sm mt-2">
-                  {event.description}
-                </p>
-              )}
-            </div>
-          </Link>
-        ))}
+        {events.length ? (
+          events.map((event: Event) => (
+            <Link href={`/events/${event.id}`} key={event.id}>
+              <div className="border rounded-2xl p-4 mb-3">
+                <h3 className="text-xl font-semibold">{event.name}</h3>
+                {event?.description && (
+                  <p className="whitespace-pre-line text-light text-sm mt-2">
+                    {event.description}
+                  </p>
+                )}
+              </div>
+            </Link>
+          ))
+        ) : (
+          <div className="mt-10 flex flex-col items-center justify-center text-center">
+            <Image
+              src="/empty-mailbox.webp"
+              alt="Empty Mailbox"
+              width={160}
+              height={160}
+              className="mb-6"
+            ></Image>
+            <h3 className="font-semibold text-xl mb-2">No Events yet</h3>
+            <p className="mb-4">Get started by creating an event.</p>
+            <Link href="events/new">
+              <Button>
+                Create Event
+                <PlusIcon />
+              </Button>
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   )
