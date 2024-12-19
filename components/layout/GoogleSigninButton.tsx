9620dcbcdfc0ab40590b017d14ee8297a7e7ccfa
Just-Hosam <hosam_firas@hotmail.com>
2024-12-19 11:39:47 -0700

    Sign in UI enhancement

--- components/layout/GoogleSigninButton.tsx
+++ components/layout/GoogleSigninButton.tsx
@@ -7,12 +7,16 @@ import Image from "next/image"
 export function GoogleSigninButton() {
   return (
     <div className="mx-auto w-fit">
-      <Button onClick={() => signIn("google")} variant="outline">
+      <Button
+        onClick={() => signIn("google")}
+        variant="outline"
+        className="p-6 gap-3 rounded-lg"
+      >
         <Image
           src="/google-logo.png"
           alt="Google Logo"
-          width={18}
-          height={18}
+          width={20}
+          height={20}
         ></Image>
         <p className="text-base">Login with Google</p>
       </Button>
